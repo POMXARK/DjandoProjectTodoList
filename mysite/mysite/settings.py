e094fa75e13558eeca92400a68afb19ace7beed8
POMXARK <1997pom@gmail.com>
2022-02-16 19:30:00 +1000

    Replace DATABASE on PostgreSQL, install Database Navigator

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,12 +76,34 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'OPTIONS': {
+    #        'service': 'my_service',
+     #       'passfile': '.my_pgpass',
+      #  },
+    #}
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_project', # название базы в pgadmin
+        'USER': 'postgres',
+        'PASSWORD': 12345678,
+        'HOST':'localhost',
+        'PORT': 5432
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
