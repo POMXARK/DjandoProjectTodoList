ffb539d55195bf95d64b09e980af63ccf2c80544
POMXARK <1997pom@gmail.com>
2022-02-16 20:02:48 +1000

    Connect DataBase with template html

--- mysite/todo_list/views.py
+++ mysite/todo_list/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 
+from .models import TodoAction
+
+
 class IndexViewTodo(generic.ListView):
+    model = TodoAction
     template_name = 'todo_list/index.html'
-    def get_queryset(self):
-        return
+    context_object_name = 'todo_actions'
+    #def get_queryset(self):
+    #    return
